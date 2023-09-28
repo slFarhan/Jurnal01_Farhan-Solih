383e6a2a981bf99c1a8c4cca1835d3fd55add3f4
slFarhanh <farhansolih0@gmail.com>
2023-09-28 14:00:28 +0700

    Nama

--- Jurnal01.java
+++ Jurnal01.java
@@ -1,7 +1,7 @@
 public class Jurnal01{
     public static void main(String[] args) {
-      System.out.println("Hello");
-      System.out.println("World");
-      
+      System.out.print("Nama" + ":");
+      System.out.println("Farhan Solih");
+
     }
 }
