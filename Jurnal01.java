8eda8ec5bef7b5f594e4229e139d5abea07721a5
slFarhanh <farhansolih0@gmail.com>
2023-09-28 14:04:52 +0700

    Spasi

--- Jurnal01.java
+++ Jurnal01.java
@@ -1,10 +1,10 @@
 public class Jurnal01{
     public static void main(String[] args) {
-      System.out.print("Nama" + ":");
+      System.out.print("Nama" + " : ");
       System.out.println("Farhan Solih");
-      System.out.print("NIM" +":" );
+      System.out.print("NIM" +" : " );
       System.out.println("607062300009");
-      System.out.print("Kelas" + ":");
+      System.out.print("Kelas" + " : ");
       System.out.println("D3IF-47-01");
    
     }
