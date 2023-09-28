280b9add2965a8d1e211e3fd9ab9af05352b5fa8
slFarhanh <farhansolih0@gmail.com>
2023-09-28 14:02:12 +0700

    Kelas

--- Jurnal01.java
+++ Jurnal01.java
@@ -2,6 +2,7 @@ public class Jurnal01{
     public static void main(String[] args) {
       System.out.print("Nama" + ":");
       System.out.println("Farhan Solih");
-
+      System.out.print("NIM" +":" );
+      System.out.println("607062300009");
     }
 }
